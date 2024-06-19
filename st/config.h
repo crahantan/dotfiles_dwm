62c14b11f68b12c3692e0b169f1526c55315b068
Crahantan <crahantandev@gmail.com>
2024-06-19 10:57:25 -0600

    Modified font and alpha

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font Regular:pixelsize=16";
+static char *font = "JetBrainsMono Nerd Font SemiBold:pixelsize=16";
 static int borderpx = 2;
 
 /*
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
