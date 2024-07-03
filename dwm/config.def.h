a1f0cda6fbb79e7c425bfc65ec08bb34aa6e39f0
Crahantan <crahantandev@gmail.com>
2024-07-03 16:22:50 -0600

    Rest

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -17,13 +17,13 @@ static const int systraypinningfailfirst =
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
-static const char *fonts[] = {"JetBrainsMono Nerd Font:size=10"};
-static const char dmenufont[] = "JetBrainsMono Nerd Font:size=10";
+static const char *fonts[] = {"JetBrainsMono Nerd Font:size=12"};
+static const char dmenufont[] = "JetBrainsMono Nerd Font:size=12";
 #include "/home/crahantan/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
-static const char *tags[] = {"", "", "", "", "",
-                             "", "", "", ""};
+static const char *tags[] = {"", "", "", "", "",
+                             "", "", "", ""};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -70,7 +70,7 @@ static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", NULL};
 static const char *rofi[] = {
-    "sh", "~/.config/rofi/launchers/type-3/launcher.sh", NULL};
+    "/home/crahantan/.config/rofi/launchers/type-6/launcher.sh", NULL};
 static const char *termcmd[] = {"kitty", NULL};
 static const char *volumeUp[] = {"pamixer", "-i", "5", NULL};
 static const char *volumeDown[] = {"pamixer", "-d", "5", NULL};
