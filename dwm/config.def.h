1339e1fb0be2fc8cc8f80fcc5111094f6c15acb6
Crahantan <crahantandev@gmail.com>
2024-07-11 16:15:46 -0600

    110724

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -17,13 +17,13 @@ static const int systraypinningfailfirst =
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
-static const char *fonts[] = {"JetBrainsMono Nerd Font SemiBold:size=12"};
-static const char dmenufont[] = "JetBrainsMono Nerd Font SemiBold:size=12";
+static const char *fonts[] = {"FontAwesome Bold:size=14"};
+static const char dmenufont[] = "FontAwesome Bold:size=14";
 #include "/home/crahantan/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
-static const char *tags[] = {"󱑷", "󱑷", "󱑷", "󱑷", "󱑷",
-                             "󱑷", "󱑷", "󱑷", "󱑷"};
+static const char *tags[] = {"", "", "", "", "",
+                             "", "", "", ""};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -32,6 +32,7 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating   monitor
        border width */
+    {"slstatus", NULL, NULL, 1 << 8, 0, -1, 1},
     {"Firefox", NULL, NULL, 1 << 8, 0, -1, 0},
 };
 
