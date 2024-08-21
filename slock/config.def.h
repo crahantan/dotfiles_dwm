59f23f9f7ade09c7f2d597c62f5c7a5fec1e160a
crahantan <crahantandev@gmail.com>
2024-08-21 14:21:35 -0600

    change image slock

--- slock/config.def.h
+++ slock/config.def.h
@@ -13,4 +13,4 @@ static const int failonclear = 1;
 
 /* Background image path, should be available to the user above */
 static const char *background_image =
-    "/home/crahantan/Imágenes/wallpapers/1332269.jpeg";
+    "/home/crahantan/Imágenes/wallpapers/1338182.png";
