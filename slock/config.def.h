3ac947804ff5012eb5b5e7adb7b67c1d9fe8ad94
crahantan <crahantandev@gmail.com>
2024-08-23 16:36:29 -0600

    Change slock image and new config volume

--- slock/config.def.h
+++ slock/config.def.h
@@ -13,4 +13,4 @@ static const int failonclear = 1;
 
 /* Background image path, should be available to the user above */
 static const char *background_image =
-    "/home/crahantan/Imágenes/wallpapers/1338182.png";
+    "/home/crahantan/Imágenes/wallpapers/1373028.png";
