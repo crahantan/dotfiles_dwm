2dda942b6508430953235290f99ea225d579ddcd
Luis Álvarez <crahantandev@gmail.com>
2024-12-13 18:57:14 -0600

    New style

--- dwm/dwm/config.def.h
+++ dwm/dwm/config.def.h
@@ -1,15 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int stairpx   = 20;       /* depth of the stairs layout */
 static const int stairdirection     = 1;        /* 0: left-aligned, 1: right-aligned */
 static const int stairsamesize      = 1;        /* 1 means shrink all the staired windows to the same size */
-static const char *fonts[]          = { "Iosevka Nerd Font Medium:size=10" };
-static const char dmenufont[]       = "Iosevka Nerd Font Medium:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Medium:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Medium:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
