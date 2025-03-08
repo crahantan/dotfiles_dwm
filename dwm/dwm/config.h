4a55b1afbeeb214cad812bb7b9a0513f813dc03f
Luis Álvarez <crahantandev@gmail.com>
2025-03-08 13:02:16 -0600

    DWM:Set correct layout

--- dwm/dwm/config.h
+++ dwm/dwm/config.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.70; /* factor of master area size [0.05..0.95] */
+static const float mfact = 0.7; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
 static const int resizehints =
     1; /* 1 means respect size hints in tiled resizals */
