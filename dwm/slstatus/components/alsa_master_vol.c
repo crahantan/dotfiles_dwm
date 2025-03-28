70620170ab782bb37daed323df0dfe43dd26d997
Luis Álvarez <crahantandev@gmail.com>
2025-03-28 11:32:02 -0600

    SLSTATUS: alsa_master_vol.c - Modified to match with output, bk directory have original script

--- dwm/slstatus/components/alsa_master_vol.c
+++ dwm/slstatus/components/alsa_master_vol.c
@@ -4,61 +4,28 @@
 #include <string.h>
 #include "../util.h"
 
-#define TMP_BUF_SIZE 14
+#define TMP_BUF_SIZE 22
 #define VOL_BUF_SIZE 5
 
 const char *
 alsa_master_vol(const char *unused)
 {
-	bool MASTER_IS_MUTED = true;
-	char tmp_buf[TMP_BUF_SIZE];
-	short b;
-	unsigned short i = 0;
-	
-	FILE *fp = popen("amixer get Master | tail -c13", "r");
-	char ch;
-	while ((ch = fgetc(fp)) != EOF && i < TMP_BUF_SIZE)
-		tmp_buf[i++] = ch;
-	tmp_buf[i] = '\0';
-	pclose(fp);
-
-	b = i - 1;
-	while (b >= 0)
-	{
-		if ('[' == tmp_buf[b])
-		{
-			if (tmp_buf[b+1] == 'o' && tmp_buf[b+2] == 'n')
-				MASTER_IS_MUTED = false;
-			b -= 3;
-			break;
-		}
-		b--;
-	}
-	
-	if (MASTER_IS_MUTED) return bprintf("MUTE");
-	else
-	{
-		char vol_buf[VOL_BUF_SIZE];
-		while (b >= 0)
-		{
-			if ('[' == tmp_buf[b])
-				break;	
-			b--;
-		}
-		
-		i = 0;
-		while (i < VOL_BUF_SIZE)
-		{
-			b++;
-			if (']' == tmp_buf[b])
-			{
-				vol_buf[i] = '\0'; 
-				break;
-			}
-			else
-				vol_buf[i++] = tmp_buf[b];
-		}
-		
-		return bprintf("%s", vol_buf);
-	}
+    char tmp_buf[TMP_BUF_SIZE];
+    unsigned short i = 0;
+    
+    FILE *fp = popen("amixer get Master | grep -o '[0-9]*%' | head -n1", "r");
+    if (!fp) return "Error";
+    
+    char vol_buf[VOL_BUF_SIZE];
+    if (fgets(vol_buf, VOL_BUF_SIZE, fp) == NULL) {
+        pclose(fp);
+        return "Error";
+    }
+    pclose(fp);
+    
+    size_t len = strlen(vol_buf);
+    if (len > 0) vol_buf[len - 1] = '\0'; // Elimina el último carácter
+    
+    return bprintf("%s", vol_buf);
 }
+
