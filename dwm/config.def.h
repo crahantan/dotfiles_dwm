21d8917796468a48b0dcd952fa2b811b3dc0dba1
Luis Alvarez <crahantandev@gmail.com>
2025-05-17 11:18:29 -0600

    Feat: dwm bar

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx = 4; /* border pixel of windows */
 static const unsigned int gappx = 20;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const int showbar = 0;           /* 0 means no bar */
+static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 0;            /* 0 means bottom bar */
 static const unsigned int stairpx = 10; /* depth of the stairs layout */
 static const int stairdirection = 1;    /* 0: left-aligned, 1: right-aligned */
