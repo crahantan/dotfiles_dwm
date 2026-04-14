cc2e82c8ca42697fbe3aed6124bd1a1c8a6d48cd
crahantan <crahantandev@gmail.com>
2026-04-14 12:56:19 -0600

    chore: change show bottom bar

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx = 10;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int rmaster =
     1; /* 1 means master-area is initially on the right */
-static const int showbar = 1;           /* 0 means no bar */
+static const int showbar = 0;           /* 0 means no bar */
 static const int topbar = 0;            /* 0 means bottom bar */
 static const unsigned int stairpx = 10; /* depth of the stairs layout */
 static const int stairdirection = 0;    /* 0: left-aligned, 1: right-aligned */
